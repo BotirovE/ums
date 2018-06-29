93363d5142cf321ff173d9222e03a80b217a2481
BotirovE <erkinboy.botirov@mail.ru>
2018-06-29 18:51:11 +0500

    beautiful numbers model, resource, api

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,14 @@ use App\Http\Resources\NewsCollection;
 use App\Http\Resources\ServicesCollection;
 use App\Http\Resources\CommunicationsCollection;
 use App\Http\Resources\InternetsCollection;
+use App\Http\Resources\BeautifulNumbersCollection;
 
 
 use App\News;
 use App\Service;
 use App\Communication;
 use App\Internet;
+use App\BeautifulNumber;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,21 +31,26 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('/news', function()
 {
-    return new NewsCollection(News::paginate());
+    return new NewsCollection(News::paginate(10));
 });
 
 Route::get('/services', function()
 {
-    return new ServicesCollection(Service::paginate());
+    return new ServicesCollection(Service::paginate(10));
 });
 
 
 Route::get('/communications', function()
 {
-    return new CommunicationsCollection(Communication::paginate());
+    return new CommunicationsCollection(Communication::paginate(10));
 });
 
 Route::get('/internets', function()
 {
-    return new InternetsCollection(Internet::paginate());
+    return new InternetsCollection(Internet::paginate(10));
+});
+
+Route::get('/beautiful-numbers', function()
+{
+    return new BeautifulNumbersCollection(BeautifulNumber::paginate(10));
 });
